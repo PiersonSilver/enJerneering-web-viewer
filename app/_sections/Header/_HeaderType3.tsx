82c847981190406d761e388e1c47d631f10e6a37
Pierson Silver <silverpierson1@gmail.com>
2025-04-15 02:12:49 -0400

    Update Header for Types 2 and 3 to work as intended

--- app/_sections/Header/_HeaderType3.tsx
+++ app/_sections/Header/_HeaderType3.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import ModalVideo from "../ModalVideo";
-import type { StaticImageData } from "next/image";
+import Image, { StaticImageData } from "next/image";
 import ButtonActions from "../ButtonActions";
 import { HeaderData } from "./types/HeaderData";
 import { classNames } from "@/_utils/helpers";
@@ -18,34 +18,36 @@ const HeaderType3: React.FC<HeaderProps> = ({ data }) => {
     buttons,
     tagLine,
     imgUrl,
-    videoUrl = "default-video.mp4",
+    isRowReverse = false,
+    backgroundColor,
+    textColor
   } = data;
 
   return (
     <div className="bg-gray-50">
       <div
         className={classNames(
-          "mx-auto max-w-[1440px] min-h-[624px] px-6 py-20 gap-10 lg:p-20 lg:flex lg:items-center lg:justify-between lg:gap-20"
+          isRowReverse ? "lg:flex-row-reverse" : "lg:flex-row",
+          "mx-auto max-w-[1440px] min-h-[624px] px-6 py-20 gap-10 lg:p-20 lg:flex lg:items-center lg:justify-between lg:gap-20" 
         )}
+        style={{backgroundColor}}
       >
-        <div className="mx-auto max-w-screen-xl lg:mx-0 flex flex-col gap-10 w-full lg:h-full lg:w-1/2">
+        <div className={classNames("mx-auto max-w-screen-xl lg:mx-0 flex flex-col gap-10 w-full lg:h-full lg:w-1/2")}>
           <div className="flex flex-col gap-6 text-left">
-            <span className="text-base uppercase font-bold">{tagLine}</span>
-            <h1>{title}</h1>
-            <h6>{subtitle}</h6>
+            <span className="text-base uppercase font-bold" style={{ color: textColor }}>{tagLine}</span>
+            <h1 style={{ color: textColor }}>{title}</h1>
+            <h6 style={{ color: textColor }}>{subtitle}</h6>
           </div>
 
           <ButtonActions buttons = {buttons}/>
         </div>
         <div className="mx-auto max-w-screen-xl mt-10 lg:h-full lg:w-1/2 lg:mt-0">
-          <ModalVideo
-            thumb={imgUrl as unknown as StaticImageData}
-            thumbWidth={600}
-            thumbHeight={464}
-            thumbAlt="Modal video thumbnail"
-            video={videoUrl}
-            videoWidth={1920}
-            videoHeight={1080}
+          <Image
+            src={imgUrl as unknown as StaticImageData}
+            width={600}
+            height={464}
+            alt="Header image"
+            className="w-full h-auto object-cover rounded-xl"
           />
         </div>
       </div>
--- app/_sections/Header/types/HeaderData.ts
+++ app/_sections/Header/types/HeaderData.ts
@@ -9,5 +9,5 @@ export type HeaderData = {
   background: string;
   backgroundColor?: string;
   textColor?: string;
-  videoUrl?: string;
+  isRowReverse?: boolean;
 };
