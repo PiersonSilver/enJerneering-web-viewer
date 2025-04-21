0668fb01d16677eb42189398b3d085a5deecd244
Payton Hutsell <payton.hutsell@gmail.com>
2025-04-21 19:01:45 -0400

    Fix ReferenceError

--- app/_providers/ProjectContext.tsx
+++ app/_providers/ProjectContext.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 "use client";
 import { createContext, useContext } from "react";
 
--- tests/FooterType.test.tsx
+++ tests/FooterType.test.tsx
@@ -2,23 +2,37 @@ import React from "react";
 import Footer from "@components/Footer";
 import { FooterData } from "@components/Footer/types/FooterData";
 import { render, screen } from "@testing-library/react";
+import { ProjectProvider } from "@providers/ProjectContext"; // import the provider
 
 describe("Footer Component", () => {
   const mockFooterData: FooterData = {
     logo: "/logo.png",
     slogan: "Test slogan",
-    socials: [{
-        name: "Facebook", url: "https://facebook.com", icon: "pi-facebook",
-        id: ""
-    }],
+    socials: [
+      {
+        name: "Facebook",
+        url: "https://facebook.com",
+        icon: "pi-facebook",
+        id: "",
+      },
+    ],
     navigation: [{ title: "Home", href: "/home" }],
     polices: [{ title: "Privacy Policy", url: "/privacy" }],
     copyRight: "© 2024 Test Company",
     showContentFlags: { slogan: "on", socials: "on", copyRight: "on" },
   };
 
+  const renderWithProviders = (ui: React.ReactElement) => {
+    return render(
+      <ProjectProvider projectId="mock-project-id">
+        {ui}
+      </ProjectProvider>
+    );
+  };
+  
+
   test("renders FooterType1 correctly", () => {
-    render(<Footer type={1} data={mockFooterData} />);
+    renderWithProviders(<Footer type={1} data={mockFooterData} />);
     expect(screen.getByText("Test slogan")).toBeInTheDocument();
     expect(screen.getByText("Home")).toBeInTheDocument();
   });
@@ -36,7 +50,9 @@ describe("Footer Component", () => {
 
   test("renders FooterType4 correctly", () => {
     render(<Footer type={4} data={mockFooterData} />);
-    expect(screen.getByText("© 2024 My Simple Page. All rights reserved.")).toBeInTheDocument();
+    expect(
+      screen.getByText("© 2024 My Simple Page. All rights reserved.")
+    ).toBeInTheDocument();
   });
 
   test("throws error for unsupported footer type", () => {
