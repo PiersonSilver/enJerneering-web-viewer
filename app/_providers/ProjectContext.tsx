8c9c3cbaa3a1bdc5942734b3afbce86c38990139
Payton Hutsell <payton.hutsell@gmail.com>
2025-04-21 23:49:58 -0400

    fix bug

--- app/_providers/ProjectContext.tsx
+++ app/_providers/ProjectContext.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-
 "use client";
+
+import React from "react";
 import { createContext, useContext } from "react";
 
 const ProjectContext = createContext<{ projectId: string } | null>(null);
